05b87f80c89f4c914bf445aac334900cdf10439b
tristan-remple <tristan.remple@outlook.com>
2024-02-29 13:48:30 -0400

    starting to add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import Cover from './Cover'
 import Portfolio from './Portfolio'
 import Contact from './Contact'
 import '../css/style.css'
+import { initializeApp, getAnalytics } from 'firebase/app'
 
 function App() {
+
+  const firebaseConfig = {
+    // apiKey: "AIzaSyCGQ0tYppWFJkuSxBhOpkH0xVDmX245Vdc",
+    // authDomain: "project-id.firebaseapp.com",
+    // databaseURL: "https://project-id.firebaseio.com",
+    // projectId: "project-id",
+    // storageBucket: "project-id.appspot.com",
+    // messagingSenderId: "637908496727",
+    appId: "1:69601742720:android:bcc231ee632f7c91da3b65",
+    measurementId: "G-694D2YNCCC"
+  }
+
+  const app = initializeApp(firebaseConfig)
+  const analytics = getAnalytics(app)
+
   return (
     <>
-      <NavBar />
+      <NavBar ga={ analytics } />
       <Cover />
       <Portfolio />
       <Contact />
--- src/Narbar.jsx
+++ src/Narbar.jsx
@@ -1,9 +1,20 @@
-const Narbar = () => {
+import { logEvent } from 'firebase/app'
+
+const Narbar = ({ ga }) => {
+
+  const navClick = (event) => {
+    const target = event.target.innerText
+    logEvent(ga, 'select_content', {
+      content_type: "section",
+      content_id: target
+    })
+  }
+
   return (
     <nav>
-      <a href="#">Summary</a>
-      <a href="#portfolio">Portfolio</a>
-      <a href="#contact">Contact</a>
+      <a href="#" onClick={ navClick } >Summary</a>
+      <a href="#portfolio" onClick={ navClick } >Portfolio</a>
+      <a href="#contact" onClick={ navClick } >Contact</a>
     </nav>
   )
 }
