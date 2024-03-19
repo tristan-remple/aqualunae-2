93b4ae5c09d91f0c1c3e84d42b5d971d205636d2
tristan-remple <tristan.remple@outlook.com>
2024-03-19 10:52:55 -0300

    added github link

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     <div id="contact-outer">
       <div id="contact">
         <h2>Contact</h2>
-        <p>I'm currently looking for a full-time Junior Developer position. If you think I'd be a good fit for your team, you can reach me by email at <a href="mailto:tristan.remple@outlook.com">tristan.remple@outlook.com</a> to schedule an interview or request my full resume. You can also find me on <a href="https://www.linkedin.com/in/aqualunae/">LinkedIn</a>.</p>
+        <p>I'm currently looking for a full-time Junior Developer position. If you think I'd be a good fit for your team, you can reach me by email at <a href="mailto:tristan.remple@outlook.com">tristan.remple@outlook.com</a> to schedule an interview or request my full resume. You can also find me on <a href="https://github.com/tristan-remple">GitHub</a> and <a href="https://www.linkedin.com/in/aqualunae/">LinkedIn</a>.</p>
       </div>
     </div>
   )
