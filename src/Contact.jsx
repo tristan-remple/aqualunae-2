5e6b9f0572fdb0c8d8a49777b0fb37d9a246cc77
tristan-remple <tristan.remple@outlook.com>
2024-03-21 10:18:09 -0300

    analytics on contact links

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
     <div id="contact-outer">
       <div id="contact">
         <h2>Contact</h2>
-        <p>I'm currently looking for a full-time Junior Developer position. If you think I'd be a good fit for your team, you can reach me by email at <a href="mailto:tristan.remple@outlook.com">tristan.remple@outlook.com</a> to schedule an interview or request my full resume. You can also find me on <a href="https://github.com/tristan-remple" onClick={ (event) => navClick(event) } >GitHub</a> and <a href="https://www.linkedin.com/in/aqualunae/" onClick={ (event) => navClick(event) } >LinkedIn</a>.</p>
+        <p>I'm currently looking for a full-time Junior Developer position. If you think I'd be a good fit for your team, you can reach me by email at <a href="mailto:tristan.remple@outlook.com" onClick={ (event) => navClick(event) } >tristan.remple@outlook.com</a> to schedule an interview or request my full resume. You can also find me on <a href="https://github.com/tristan-remple" onClick={ (event) => navClick(event) } >GitHub</a> and <a href="https://www.linkedin.com/in/aqualunae/" onClick={ (event) => navClick(event) } >LinkedIn</a>.</p>
       </div>
     </div>
   )
