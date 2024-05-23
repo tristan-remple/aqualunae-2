1af8595eeb5abd594af505f7ffa98553d36dd992
tristan-remple <tristan.remple@outlook.com>
2024-05-23 14:45:20 -0400

    updated portfolio

--- src/Cover.jsx
+++ src/Cover.jsx
@@ -1,8 +1,9 @@
 const Cover = () => {
 
     const skills = [
-        "reactJS", "expressJS", "mongoDB", "nodeJS", "JavaScript",
-        "Laravel", "PHP", "MySQL", "AWS", "cPanel",
+        "reactJS", "ReactNative", "expressJS", "mongoDB", "nodeJS", "TypeScript", "JavaScript", 
+        "Laravel", "PHP", "WordPress", "MySQL",
+        "AWS", "cPanel", "C#", "dotNET", "GoogleScript", "GoogleAnalytics",
         "SCSS", "Bootstrap", "WCAG", "CSS", "HTML"
     ]
 
@@ -11,10 +12,10 @@ const Cover = () => {
             <img id="logo" src="../img/aqualunae-fade.png" alt="A round logo depicting a silver full moon and a wave." />
             <div id="cover-info">
                 <p>
-                    <span className="emphasis">Tristan</span> is a second year Web Programming student at NSCC. He's a driven problem solver and an artist at heart, and web development has proven to be a good mix of those things. He's fascinated by the stories and visions behind businesses and organizations, and excited to work together with clients and coworkers to translate that vision into a cohesive and user-friendly web presence. He prides himself on taking the initiative to get things done and having the attention to detail to get them done right.
+                    <span className="emphasis">Tristan</span> is a 2024 graduate of the Web Programming diploma at NSCC. He's a driven problem solver and an artist at heart, and web development has proven to be a good mix of those things. He's fascinated by the stories and visions behind businesses and organizations, and excited to work together with clients and coworkers to translate that vision into a cohesive and user-friendly web presence. He prides himself on taking the initiative to get things done and having the attention to detail to get them done right.
                 </p>
                 <div className="tech-row">
-                    { skills.map(sk => <img key={sk} className="tech-icon" src={`../img/skills/${sk}.png`} alt={`${sk} icon`} title={sk} />) }
+                    { skills.map(sk => <img key={sk} className="tech-icon" src={`../img/skills/${ encodeURIComponent(sk) }.png`} alt={`${sk} icon`} title={sk} />) }
                 </div>
             </div>
         </div>
